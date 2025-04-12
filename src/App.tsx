22ccd7eca3bd2426a1998632be7bc341d4e265b6
yashchhatrala25 <yashchhatrala25@gmail.com>
2025-04-12 16:25:42 +0530

    product page complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import MainContent from "./components/MainContent";
+import ProductPage from "./components/ProductPage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <div className="rounded w-full flex justify-between flex-wrap">
           <Routes>
             <Route path="/" element={<MainContent />} />
+            <Route path="/product/:id" element={<ProductPage />} />
           </Routes>
         </div>
       </div>
